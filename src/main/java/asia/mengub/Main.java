a5aef5ee333c4e1c73d7435a2c909acd86524b93
“Mengub” <lin_prui@163.com>
2024-01-02 14:43:34 +0800

    第一次提交

---
+++ src/main/java/asia/mengub/Main.java
@@ -0,0 +1,12 @@
+package asia.mengub;
+
+/**
+ * @Author Mengub
+ * @Date 2024/1/2
+ * @Description 笔记运行的入口文件
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
